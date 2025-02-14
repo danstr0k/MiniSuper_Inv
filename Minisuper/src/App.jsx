49ac952b85830c65996385874c96e4143f9b0245
JuanD1P <juanndieg2005@gmail.com>
2025-02-14 14:13:16 -0500

    CAMBIOS

--- Minisuper/src/App.jsx
+++ Minisuper/src/App.jsx
@@ -1,14 +1,22 @@
-import { useState } from 'react'
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import Inicio from './assets/Componentes/Inicio'
+import Registro from './assets/Componentes/Registro';
+import Crear from './assets/Componentes/Crear';
+
 
 
 function App() {
-
   return (
-    <>
-    <Inicio/>
-    </>
-  )
+      <Router>
+          <Routes>
+              <Route path="/" element={<Navigate to="/Inicio" />} />
+              <Route path="/Inicio" element={<Inicio/>} />
+              <Route path="/Registro" element={<Registro /> } />
+              <Route path="/Crear" element={<Crear />} />
+          </Routes>
+      </Router>
+  );
 }
 
 export default App
--- Minisuper/src/assets/Componentes/Inicio.jsx
+++ Minisuper/src/assets/Componentes/Inicio.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import React from 'react';
 
 const Inicio = () => {
   return (
-    <div>Inicio </div>
-  )
-}
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-900 text-white">
+      <h1 className="text-4xl font-bold mb-6">Bienvenido a Jumanji</h1>
+      <div className="space-x-4">
+        <a href="/inicio" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Inicio</a>
+        <a href="/registro" className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded">Registro</a>
+      </div>
+    </div>
+  );
+};
 
-export default Inicio
+export default Inicio;
